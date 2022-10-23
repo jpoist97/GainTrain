26943292b05d4395faa7d74f1db17b4a50367999
Justin Poist <42504462+jpoist97@users.noreply.github.com>
2022-10-22 22:55:05 -0700

    Add notification button

---
+++ gaintrain-fe/components/log-workout/notification-button.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import * as Notifications from 'expo-notifications';
+import { Text, TouchableOpacity } from 'react-native';
+
+const NotificationButton = () => {
+    const scheduleNotification = (seconds) => {
+        Notifications.scheduleNotificationAsync({
+            content: {
+              title: "Rest timer is up",
+              body: 'Start your new set!',
+            },
+            trigger: {
+              seconds,
+            },
+          });
+    }
+
+    return (
+        <TouchableOpacity onPress={() => {scheduleNotification(70)}}>
+            <Text>Schedule Notification</Text>
+        </TouchableOpacity>
+    );
+}
+
+export default NotificationButton;
